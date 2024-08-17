2279b5f8798a2355eaa1ff20ab58a1101f5d9227
Lana <Lana@DESKTOP-EN0U2RN>
2024-08-17 13:56:08 +0300

    first commit

---
+++ Artisan.Infrastructure/ArtisanDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Artisan.Domain;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Artisan.Domain.Entities; 
+namespace Artisan.Infrastructure.Data
+{ 
+    public class ArtisanDBContext : DbContext
+    {
+        public ArtisanDBContext(DbContextOptions<ArtisanDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Artisans> Artisans{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+    }
+}
